55654e3a7ea03e0f5d72d0123ac7af2785d89eb9
Sebastián Mastrángelo <sebba.git@gmail.com>
2025-10-15 21:52:41 -0300

    add: class to h1-p. Styles: colors padding modified - add concat with js

---
+++ JS/products.js
@@ -0,0 +1,6 @@
+let products = document.querySelector('.sect__products');
+
+let newProduct = document.createElement('div');
+newProduct.setAttribute("class", ".container__img-products");
+
+products.appendChild(newProduct);

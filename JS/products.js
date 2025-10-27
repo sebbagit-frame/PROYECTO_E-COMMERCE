cce97a9a13b821bc9c0a299f50fc43cc288e5bfc
Sebastián Mastrángelo <sebba.git@gmail.com>
2025-10-26 22:32:17 -0300

    img's modified - add: 'product.js' for function and DOM. add: remove file- img and add new img. edit style product's

--- JS/products.js
+++ JS/products.js
@@ -1,6 +1,125 @@
-let products = document.querySelector('.sect__products');
+const productos = [
+    {
 
-let newProduct = document.createElement('div');
-newProduct.setAttribute("class", ".container__img-products");
+        // ABRIGOS
+        id: "buzo-brucs",
+        titulo: "Buzo Brucs - Algodón suave con capucha ajustable.",
+        imagen: "IMG/productos/abrigos/buzoBrucs_ok.png",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
 
-products.appendChild(newProduct);
+        id: "buzo-carmel",
+        titulo: "Buzo Carmel Bensimon - Algodón rústico",
+        imagen: "IMG/productos/abrigos/buzo-carmel_ok.png",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+
+        id: "buzo-over-essent",
+        titulo: "Buzo Oversize - Estampa bordado",
+        imagen: "IMG/productos/abrigos/buzo-over-essential_ok.png",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+
+
+        // REMERAS
+        id: "remera-nevada",
+        titulo: "Remera Nevada - Lisa sin estampa",
+        imagen: "IMG/productos/remeras/remera-nevada_ok.png",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+
+        id: "remera-brucs",
+        titulo: "Remera Brucs - 100% Algodón",
+        imagen: "IMG/productos/remeras/remera_front-brucs.png",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+
+        id: "remera-overbox",
+        titulo: "Remera Overbox - Mezcla poliéster",
+        imagen: "IMG/productos/remeras/remera-overbox-fern_ok.png",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+
+
+        // PANTALONES
+        id: "cargo-brucs",
+        titulo: "Cargo Brucs - múltiples bolsillos",
+        imagen: "IMG/productos/pantalones/cargoAlgodon-Brucs",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+
+        id: "cargo-comdecevis",
+        titulo: "Cargo Gabardina - Comdecevis",
+        imagen: "IMG/productos/pantalones/cargoGabardina-Comdecevis.png",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+
+        
+        // ZAPATILLAS
+        id: "zapatilla-brucs",
+        titulo: "Zapatillas con suela de goma antideslizante",
+        imagen: "IMG/productos/zapatillas/zapt-brucs.jpg",
+        categoria: {
+            nombre: "Zapatillas",
+            id: "zapatillas"
+        },
+
+
+        // GORRAS
+
+        id: "gorra-brucs",
+        titulo: "Gorra con visera curva, ajustable",
+        imagen: "IMG/productos/gorras/gorra_brucs.png",
+        categoria: {
+            nombre: "Gorras",
+            id: "gorras"
+        },
+
+
+        // PUPERAS
+
+        id: "pupera-brucs",
+        titulo: "Pupera confeccionada en algodón con elastano",
+        imagen: "IMG/productos/puperas/pupera-mujer-oversize_ok.png",
+        categoria:{
+            nombre: "Puperas",
+            id: "puperas"
+        },
+
+        id: "pupera-convoy",
+        titulo: "Pupera Convoy Rock Style",
+        imagen: "IMG/productos/puperas/pupera-convoy.png",
+        categoria: {
+            nombre: "Puperas",
+            id: "puperas"
+        }
+
+
+    }
+]
+
+
+const contProducts = document.querySelector("#contenedor-productos");
+
+
+function uploadProducts() {
+    
+
+
+}
